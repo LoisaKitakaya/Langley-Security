4a1d51f089f6550c163a960a81ea68dc01878ac2
Freedom Loisa <kitakayaloisa@gmail.com>
2022-08-16 14:41:56 +0300

    added react player and styled home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import { Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Container, Button } from "semantic-ui-react";
+import ReactPlayer from "react-player/youtube";
 
 import PageTitle from "../components/PageTitle";
 
@@ -9,9 +11,36 @@ const Home = () => {
 
   return (
     <div className="page">
+      {/* intro */}
       <Container>
-        <p>This is the home page</p>
+        <div className="is-flex intro-container">
+          <div>
+            <ReactPlayer url="https://www.youtube.com/watch?v=ug50zmP9I7s" />
+          </div>
+          <div className="has-background-light`">
+            <div className="intro-content">
+              <h1 className="is-size-1">There for you</h1>
+              <br />
+              <p className="is-size-5">
+                Since 2012, we have forged our organization under a single
+                purpose: to keep people, businesses, and communities safe so we
+                can all thrive together.
+              </p>
+              <br />
+              <Button.Group>
+                <Link to={"/who-we-are"} className="button is-danger">
+                  Who we are
+                </Link>
+                <Link to={"/what-we-do"} className="button is-danger">
+                  What we do
+                </Link>
+              </Button.Group>
+              <br />
+            </div>
+          </div>
+        </div>
       </Container>
+      {/* into */}
     </div>
   );
 };
