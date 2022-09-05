d6ba28a09689ab8a432fea2828591569526c2500
Freedom Loisa <kitakayaloisa@gmail.com>
2022-09-05 11:40:28 +0300

    updated privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -82,16 +82,12 @@ const PrivacyPolicy = () => {
               </li>
               <li>
                 <Icon name="angle right" />{" "}
-                <a href={`#${data.navigation.idEight}`}>Contacting Us</a>
-              </li>
-              <li>
-                <Icon name="angle right" />{" "}
-                <a href={`#${data.navigation.idNine}`}>Contact Us</a>
+                <a href={`#${data.navigation.idEight}`}>Contact Us</a>
               </li>
             </ul>
           </div>
-          <Divider />
-          <div className="mb-3" id={data.navigation.idOne}>
+          <Divider id={data.navigation.idOne} />
+          <div className="mb-3">
             <p className="is-size-5 has-text-dark">
               {data.informationWeCollectAboutYou.title}
             </p>
@@ -165,6 +161,195 @@ const PrivacyPolicy = () => {
                 }
               </li>
             </ul>
+            <Divider />
+            <div className="mb-3 ml-6 mr-6 reframe">
+              <p className="is-size-5 has-text-dark">
+                {data.informationWeCollectAboutYouFromOtherSources.title}
+              </p>
+              <br />
+              <p>
+                {data.informationWeCollectAboutYouFromOtherSources.paragraph}
+              </p>
+            </div>
+            <Divider />
+            <div className="mb-3 ml-6 mr-6 reframe">
+              <p className="is-size-5 has-text-dark">
+                {data.informationWeCollectAutomatically.title}
+              </p>
+              <br />
+              <p>{data.informationWeCollectAutomatically.paragraph}</p>
+            </div>
+            <Divider />
+            <div className="mb-3 ml-6 mr-6 reframe">
+              <p className="is-size-5 has-text-dark">
+                {data.informationWeCollectInOurCapacityAsAServiceProvider.title}
+              </p>
+              <br />
+              <p>
+                {
+                  data.informationWeCollectInOurCapacityAsAServiceProvider
+                    .paragraph
+                }
+              </p>
+            </div>
+          </div>
+          <Divider id={data.navigation.idTwo} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">
+              {data.ourUseOfYourInformation.title}
+            </p>
+            <br />
+            <p>{data.ourUseOfYourInformation.setup}</p>
+            <br />
+            <ul>
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointOne}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointTwo}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointThree}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointFour}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointFive}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointSix}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointSeven}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointEight}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointNine}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointTen}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourUseOfYourInformation.delivery.pointEleven}
+              </li>
+              <br />
+            </ul>
+          </div>
+          <Divider id={data.navigation.idThree} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">
+              {data.ourDisclosureOfYourInformation.title}
+            </p>
+            <br />
+            <p>{data.ourDisclosureOfYourInformation.setup}</p>
+            <br />
+            <ul>
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourDisclosureOfYourInformation.delivery.pointOne}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourDisclosureOfYourInformation.delivery.pointTwo}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourDisclosureOfYourInformation.delivery.pointThree}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourDisclosureOfYourInformation.delivery.pointFour}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourDisclosureOfYourInformation.delivery.pointFive}
+              </li>
+              <br />
+              <li>
+                <Icon name="angle right" />{" "}
+                {data.ourDisclosureOfYourInformation.delivery.pointSix}
+              </li>
+              <br />
+            </ul>
+          </div>
+          <Divider id={data.navigation.idFour} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">
+              {data.cookiesAndTrackingTechnologies.title}
+            </p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphOne}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphTwo}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphThree}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphFour}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphFive}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphSix}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphSeven}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphEight}</p>
+            <br />
+            <p>{data.cookiesAndTrackingTechnologies.paragraphNine}</p>
+            <br />
+          </div>
+          <Divider id={data.navigation.idFive} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">{data.links.title}</p>
+            <br />
+            <p>{data.links.paragraph}</p>
+          </div>
+          <Divider id={data.navigation.idSix} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">
+              {data.optingOutOfEmailMarketing.title}
+            </p>
+            <br />
+            <p>{data.optingOutOfEmailMarketing.paragraph}</p>
+          </div>
+          <Divider id={data.navigation.idSeven} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">{data.security.title}</p>
+            <br />
+            <p>{data.security.paragraph}</p>
+          </div>
+          <Divider id={data.navigation.idEight} />
+          <div className="mb-3">
+            <p className="is-size-5 has-text-dark">{data.contactUs.title}</p>
+            <br />
+            <p>{data.contactUs.paragraph}</p>
           </div>
         </div>
       </Container>
